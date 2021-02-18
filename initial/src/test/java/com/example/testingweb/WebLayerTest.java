168006fcf3d7e145237e3442f1e4eeaf9a98cc16
major1xu <minjie.xu@gmail.com>
2021-02-18 17:02:14 -0600

    fix one test

--- initial/src/test/java/com/example/testingweb/WebLayerTest.java
+++ initial/src/test/java/com/example/testingweb/WebLayerTest.java
@@ -1,6 +1,7 @@
 package com.example.testingweb;
 
 import static org.hamcrest.Matchers.containsString;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -9,6 +10,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest
@@ -17,8 +19,13 @@ public class WebLayerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private GreetingService service;
+
     @Test
     public void shouldReturnDefaultMessage() throws Exception {
+        when(service.greet()).thenReturn("Hello, World");
+
         this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
                 .andExpect(content().string(containsString("Hello, World")));
     }
